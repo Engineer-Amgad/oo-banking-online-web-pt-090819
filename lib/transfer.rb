e5b9c5f67d603734267a9b794c51ab24534350a6
Amgad Malik <amgadmalik@yahoo.com>
2019-10-14 23:59:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,10 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status
+  attr_accessor :sender, :receiver, :status, :account
   
   def initialize(sender, receiver,status)
     @status = "pending"
     @sender = sender
     @receiver = receiver
+    @account = account
   end 
 end
