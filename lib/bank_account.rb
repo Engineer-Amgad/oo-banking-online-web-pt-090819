56ffb94be7cd0c651734842e1b3b892e10f1274b
Amgad Malik <amgadmalik@yahoo.com>
2019-10-14 23:30:49 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
-  attr_accessor :name
+  attr_accessor :name, :status
   attr_reader :balance
   
   def initialize(name)
     @name = name
     @balance = 1000
+    @status = "open"
     
   end 
 end
